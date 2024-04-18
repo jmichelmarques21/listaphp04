f43fddb11500cc5f7aa2fef529c6523f0fa86ca8
jmichelmarques21 <jmichelmarques21@gmail.com>
2024-04-18 00:04:18 -0300

    commit index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lista Exercícios 04</title>
+</head>
+<body>
+<p><h1>Lista 2 de Exercicios em PHP</h1></p>
+<a href="Lista 04 - PHP.pdf">Lista 04 - Exercícios PHP</a>
+<a href="exercicio01.php"><h3>Exercício 01</h3></a>
+<a href="exercicio02.php"><h3>Exercício 02</h3></a>
+<a href="exercicio03.php"><h3>Exercício 03</h3></a>
+<a href="exercicio04.php"><h3>Exercício 04</h3></a>
+<a href="exercicio05.php"><h3>Exercício 05</h3></a>
+<a href="exercicio06.php"><h3>Exercício 06</h3></a>
+<a href="exercicio07.php"><h3>Exercício 07</h3></a>
+</body>
+</html>
