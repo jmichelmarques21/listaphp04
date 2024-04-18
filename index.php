5acfc786dce354122b6eeda4bc69fb8d8dde71a3
jmichelmarques21 <jmichelmarques21@gmail.com>
2024-04-18 00:12:19 -0300

    commit index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Lista Exercícios 04</title>
+  <title>Lista Exercícios 4</title>
 </head>
 <body>
-<p><h1>Lista 2 de Exercicios em PHP</h1></p>
+<p><h1>Lista 4 de Exercicios em PHP</h1></p>
 <a href="Lista 04 - PHP.pdf">Lista 04 - Exercícios PHP</a>
 <a href="exercicio01.php"><h3>Exercício 01</h3></a>
 <a href="exercicio02.php"><h3>Exercício 02</h3></a>
@@ -15,5 +15,13 @@
 <a href="exercicio05.php"><h3>Exercício 05</h3></a>
 <a href="exercicio06.php"><h3>Exercício 06</h3></a>
 <a href="exercicio07.php"><h3>Exercício 07</h3></a>
+<a href="exercicio08.php"><h3>Exercício 08</h3></a>
+<a href="exercicio09.php"><h3>Exercício 09</h3></a>
+<a href="exercicio10.php"><h3>Exercício 10</h3></a>
+<a href="exercicio11.php"><h3>Exercício 11</h3></a>
+<a href="exercicio12.php"><h3>Exercício 12</h3></a>
+<a href="exercicio13.php"><h3>Exercício 13</h3></a>
+<a href="exercicio14.php"><h3>Exercício 14</h3></a>
+<a href="exercicio15.php"><h3>Exercício 15</h3></a>
 </body>
 </html>
